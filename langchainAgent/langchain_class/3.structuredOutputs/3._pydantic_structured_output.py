0ab02d9bb7db14acdfb115bd84933cb66efb3bfe
Muhammad Ikram <ikram2500@gmail.com>
2026-05-01 07:31:06 +0500

    pydantic structured output

---
+++ langchainAgent/langchain_class/3.structuredOutputs/3._pydantic_structured_output.py
@@ -0,0 +1,38 @@
+from langchain_google_genai import ChatGoogleGenerativeAI 
+from dotenv import load_dotenv 
+from typing import TypedDict , Annotated , Literal, Optional, List 
+from pydantic import BaseModel , Field 
+
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model = "gemini-2.5-flash")
+
+class Review(BaseModel):
+    key_themes:List[str] = Field(description="Write down 3 keys thems discussed in the revie in a list")
+    summary : str = Field(description="A brief summary of the review")
+    sentiment: Literal["positive", "negative"] = Field(description="Return the sentiment of the review either positive or negative")
+    name: Optional[str] = Field(description="write down the name of the reviewer")
+
+st_model = model.with_structured_output(Review)
+
+prompt="""
+Google's Pixel phone have never been the most powerful hundreds. With their Tensor chipsets falling behind rivals in bnchmarks. But surprisingly, the Google Pixel 10 series might be even more compromised than 
+the Pixel 9 series, at least when it comes to the GPU (Graphical Processing unit). 
+
+In a post on X, @lafaiel (via Phone Arena) shared a screenshot of a Geekbench listing for the Google Pixel 10 Pro, in which the Pixel 10 Pro achieved a GPU score of just 3,707.
+Higher is better here, and for comparison, the Pixel 9 Pro's score is 9,023, while rivals like the Samsung Galaxy S25 Plus and iPhone 16 Pro achieve scores of 26,333 and 33,374
+respectively.
+
+So based on this result the Pixel 10 Pro's is way behind, though the fact that it scored even less than its predecessor is especially worryin. 
+Performance upgrades in othere areas
+
+Now, GPU performance is only one part of the power picture, and the Pixel 10 Pro should outperform the Pixel 9 Pro on Geekbench overall, with the same source recording a single core recording a single
+core score of 2,329 and a multi-core result of 6,358,  for the similarly spec'd Google Pixel 10 Pro XL, compared to 1,948 and 4,530 fir single and multi-core respectively on the 
+Pixel 9 Pro.
+"""
+
+
+result = st_model.invoke(prompt)
+
+print(result)
