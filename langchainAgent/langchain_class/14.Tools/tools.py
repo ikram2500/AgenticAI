47451d06739bb27e39f1e7033a2898576beeb647
Muhammad Ikram <ikram2500@gmail.com>
2026-05-05 19:41:04 +0500

    tool added

---
+++ langchainAgent/langchain_class/14.Tools/tools.py
@@ -0,0 +1,11 @@
+from langchain_core.tools import tool
+
+
+@tool
+def multiply(a:int, b: int):
+    """Multiply two numbers"""
+    return a*b 
+
+result = multiply.invoke({"a":4, "b":5})
+
+print(result)
